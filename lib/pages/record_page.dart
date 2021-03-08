68088787ee46b18a0f04b877d1d5dd5b927003a3
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-03-09 06:56:44 +0900

    created a functioning tab page

---
+++ lib/pages/record_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Record'),
+      ),
+    );
+  }
+}
--- lib/pages/tab_page.dart
+++ lib/pages/tab_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:spot/pages/tabs/map_tab.dart';
+import 'package:spot/pages/tabs/profile_tab.dart';
 
-class TabPage extends StatelessWidget {
+enum _Tab {
+  map,
+  profile,
+}
+
+class TabPage extends StatefulWidget {
   static Route<void> route() {
     return MaterialPageRoute(builder: (_) => TabPage());
   }
 
   @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  _Tab _currentTab = _Tab.map;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentTab.index,
+        children: [
+          MapTab(),
+          ProfileTab(),
+        ],
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _bottomNavigationButton(_Tab.map),
+          _bottomNavigationButton(_Tab.profile),
+        ],
+      ),
+    );
+  }
+
+  IconButton _bottomNavigationButton(_Tab tab) {
+    late IconData iconData;
+    if (tab == _Tab.map) {
+      iconData = Icons.home;
+    } else if (tab == _Tab.profile) {
+      iconData = Icons.person;
+    }
+    return IconButton(
+      icon: Icon(iconData),
+      onPressed: () {
+        setState(() {
+          _currentTab = tab;
+        });
+      },
+    );
   }
 }
