fcce5e24c11e0c733cbb7cedd62b39e383521357
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-03-16 12:35:58 +0900

    installed google map plugin

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDe5Zyx6Rb4qrY3wt2Rsf9MfE1prt0vPcM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/pages/tabs/map_tab.dart
+++ lib/pages/tabs/map_tab.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: const CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(37.43296265331129, -122.08832357078792),
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        // _controller.complete(controller);
+      },
+    );
   }
 }
