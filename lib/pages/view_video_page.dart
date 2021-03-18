c6877440a36703b017c98db55d09d6d2a40369bb
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-03-18 20:57:16 +0900

    design fix

--- lib/pages/view_video_page.dart
+++ lib/pages/view_video_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spot/app/constants.dart';
 import 'package:spot/cubits/video/video_cubit.dart';
+import 'package:spot/models/video.dart';
 import 'package:video_player/video_player.dart';
 
 import '../components/app_scaffold.dart';
@@ -25,104 +26,128 @@ class ViewVideoPage extends StatelessWidget {
             return preloader;
           } else if (state is VideoLoading) {
             final video = state.video;
-            return Image.network(
-              video.thumbnailUrl,
-              fit: BoxFit.cover,
+            return _VideoScreen(
+              video: video,
             );
           } else if (state is VideoPlaying) {
             final controller = state.videoPlayerController;
             final video = state.video;
-            return Stack(
-              fit: StackFit.expand,
-              children: [
-                ClipRect(
-                  child: OverflowBox(
-                    alignment: Alignment.center,
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: SizedBox(
-                        height: 1,
-                        child: AspectRatio(
-                          aspectRatio: controller.value.aspectRatio,
-                          child: VideoPlayer(controller),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 18,
-                  top: MediaQuery.of(context).padding.top + 18,
-                  child: const BackButton(),
-                ),
-                Positioned(
-                  bottom: 123,
-                  right: 14,
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: SizedBox(
-                      width: 36,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ClipOval(
-                            child: Image.network(
-                              'https://www.muscleandfitness.com/wp-content/uploads/2015/08/what_makes_a_man_more_manly_main0.jpg?quality=86&strip=all',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const SizedBox(height: 36),
-                          IconButton(
-                            icon: const Icon(Icons.comment),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(height: 36),
-                          IconButton(
-                            icon: const Icon(Icons.favorite),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(height: 36),
-                          IconButton(
-                            icon: const Icon(Icons.more_horiz),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                    left: 19,
-                    bottom: 50,
-                    child: SizedBox(
-                      width: 195,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            '@${video.createdBy.name}',
-                            style: const TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            video.description,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              height: 1.3,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )),
-              ],
+            return _VideoScreen(
+              controller: controller,
+              video: video,
             );
           }
           return Container();
         },
       ),
+    );
+  }
+}
+
+class _VideoScreen extends StatelessWidget {
+  const _VideoScreen({
+    Key? key,
+    this.controller,
+    required this.video,
+  }) : super(key: key);
+
+  final VideoPlayerController? controller;
+  final Video video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        controller == null
+            ? Image.network(
+                video.thumbnailUrl,
+                fit: BoxFit.cover,
+              )
+            : ClipRect(
+                child: OverflowBox(
+                  alignment: Alignment.center,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      height: 1,
+                      child: AspectRatio(
+                        aspectRatio: controller!.value.aspectRatio,
+                        child: VideoPlayer(controller!),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+        Positioned(
+          left: 18,
+          top: MediaQuery.of(context).padding.top + 18,
+          child: const BackButton(),
+        ),
+        Positioned(
+          bottom: 123,
+          right: 14,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: SizedBox(
+              width: 36,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ClipOval(
+                    child: Image.network(
+                      'https://www.muscleandfitness.com/wp-content/uploads/2015/08/what_makes_a_man_more_manly_main0.jpg?quality=86&strip=all',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 36),
+                  IconButton(
+                    icon: const Icon(Icons.comment),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(height: 36),
+                  IconButton(
+                    icon: const Icon(Icons.favorite),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(height: 36),
+                  IconButton(
+                    icon: const Icon(Icons.more_horiz),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 19,
+          bottom: 50,
+          child: SizedBox(
+            width: 195,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '@${video.createdBy.name}',
+                  style: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  video.description,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    height: 1.3,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
