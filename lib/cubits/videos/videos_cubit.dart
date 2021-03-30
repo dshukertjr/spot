a18e2cddc09104f2a07bc0f2b69a766fbe51a5fa
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-03-31 06:46:08 +0900

    Getting user's current location

--- lib/cubits/videos/videos_cubit.dart
+++ lib/cubits/videos/videos_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:spot/models/profile.dart';
@@ -47,8 +48,44 @@ class VideosCubit extends Cubit<VideosState> {
   ];
 
   Future<void> initialize() async {
+    final position = await _determinePosition();
     // TODO receive geo point and get videos
     await Future.delayed(const Duration(seconds: 1));
     emit(VideosLoaded(_videos));
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    } // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return Geolocator.getCurrentPosition();
   }
 }
