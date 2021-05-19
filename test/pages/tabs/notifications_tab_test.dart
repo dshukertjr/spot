8a1e01b06ba54b9b00e29052b11b33ec0f917394
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-05-19 17:08:34 +0900

    Created tests to make sure the new notification dot will show up in notifications tab [no ci]

--- test/pages/tabs/notifications_tab_test.dart
+++ test/pages/tabs/notifications_tab_test.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:spot/app/constants.dart';
 import 'package:spot/components/app_scaffold.dart';
+import 'package:spot/components/notification_dot.dart';
+import 'package:spot/cubits/notification/notification_cubit.dart';
 import 'package:spot/models/notification.dart';
 import 'package:spot/pages/tabs/notifications_tab.dart';
 
@@ -26,7 +29,7 @@ void main() {
               targetVideoThumbnail: 'https://dshukertjr.dev/images/profile.jpg',
               actionUid: 'aaa',
               actionUserName: 'Tyler',
-              isNew: false,
+              isNew: true,
             ),
             AppNotification(
               type: NotificationType.follow,
@@ -48,7 +51,10 @@ void main() {
           ]));
 
       await tester.pumpApp(
-        widget: AppScaffold(body: NotificationsTab.create()),
+        widget: BlocProvider<NotificationCubit>(
+          create: (context) => NotificationCubit(repository: repository)..loadNotifications(),
+          child: AppScaffold(body: NotificationsTab.create()),
+        ),
         repository: repository,
       );
 
@@ -59,13 +65,65 @@ void main() {
 
       expect(find.byWidget(preloader), findsNothing);
       expect(find.byType(ListView), findsOneWidget);
-      find
-        ..byWidgetPredicate((widget) =>
-            widget is RichText && widget.text.toPlainText().contains('liked your video'))
-        ..byWidgetPredicate(
-            (widget) => widget is RichText && widget.text.toPlainText().contains('commented'))
-        ..byWidgetPredicate((widget) =>
-            widget is RichText && widget.text.toPlainText().contains('started following you'));
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is RichText && widget.text.toPlainText().contains('liked your video')),
+          findsOneWidget);
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is RichText && widget.text.toPlainText().contains('liked your video')),
+          findsOneWidget);
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is RichText && widget.text.toPlainText().contains('started following you')),
+          findsOneWidget);
+    });
+
+    testWidgets('Takes care of new notification properly', (tester) async {
+      final repository = MockRepository();
+
+      when(repository.getNotifications).thenAnswer((invocation) => Future.value([
+            AppNotification(
+              type: NotificationType.like,
+              createdAt: DateTime.now(),
+              targetVideoId: '',
+              targetVideoThumbnail: 'https://dshukertjr.dev/images/profile.jpg',
+              actionUid: 'aaa',
+              actionUserName: 'Tyler',
+              isNew: true,
+            ),
+            AppNotification(
+              type: NotificationType.follow,
+              createdAt: DateTime.now(),
+              actionUid: 'aaa',
+              actionUserName: 'Tyler',
+              isNew: false,
+            ),
+            AppNotification(
+              type: NotificationType.comment,
+              createdAt: DateTime.now(),
+              targetVideoId: '',
+              targetVideoThumbnail: 'https://dshukertjr.dev/images/profile.jpg',
+              actionUid: 'aaa',
+              actionUserName: 'Tyler',
+              commentText: 'hey',
+              isNew: false,
+            ),
+          ]));
+
+      await tester.pumpApp(
+        widget: BlocProvider<NotificationCubit>(
+          create: (context) => NotificationCubit(repository: repository)..loadNotifications(),
+          child: AppScaffold(body: NotificationsTab.create()),
+        ),
+        repository: repository,
+      );
+
+      expect(find.byWidget(preloader), findsOneWidget);
+
+      await tester.pump();
+
+      expect(find.byType(NotificationDot), findsOneWidget);
     });
   });
 }
