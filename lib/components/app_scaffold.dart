166852d50091ee9b648094f878b34e88cf8112e9
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-05-21 01:37:12 +0900

    keyboard appearance will no longer shrink google map [no ci]

--- lib/components/app_scaffold.dart
+++ lib/components/app_scaffold.dart
@@ -31,5 +31,6 @@ class AppScaffold extends Scaffold {
           floatingActionButton: floatingActionButton,
           bottomNavigationBar: bottomNavigationBar,
           extendBodyBehindAppBar: true,
+          resizeToAvoidBottomInset: false,
         );
 }
