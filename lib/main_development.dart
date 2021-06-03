890fe18223fa1abe13d090d5cda349d0f331d98f
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-06-03 20:02:57 +0900

    Added firebase to android

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -8,12 +8,15 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:bloc/bloc.dart';
 import 'package:spot/app/app.dart';
 import 'package:spot/app/app_bloc_observer.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = AppBlocObserver();
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
