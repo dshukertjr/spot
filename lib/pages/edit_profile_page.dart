50937eac45880db0f8a7dc9c6c534cb32f6f63c0
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-06-06 12:52:21 +0900

    Set route name for every page

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -18,11 +18,14 @@ class EditProfilePage extends StatefulWidget {
 
   final bool isCreatingAccount;
 
+  static const name = 'EditProfilePage';
+
   static Route<void> route({
     required bool isCreatingAccount,
     required String uid,
   }) {
     return MaterialPageRoute(
+      settings: const RouteSettings(name: name),
       builder: (context) => BlocProvider<ProfileCubit>(
         create: (context) => ProfileCubit(
           repository: RepositoryProvider.of<Repository>(context),
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,8 +5,13 @@ import 'package:spot/components/user_profile.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage(this._userId, {Key? key}) : super(key: key);
 
+  static const name = 'ProfilePage';
+
   static Route<void> route(String userId) {
-    return MaterialPageRoute(builder: (_) => ProfilePage(userId));
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: name),
+      builder: (_) => ProfilePage(userId),
+    );
   }
 
   final String _userId;
